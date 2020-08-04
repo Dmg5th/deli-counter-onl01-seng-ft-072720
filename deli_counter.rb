91e400fd22f567136b74ce50bcf32f855a221dcb
Daniel Gould <dmgould310@gmail.com>
2020-08-04 12:47:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,32 +1,32 @@
 require 'pry'
 
-def line(line)
-  current_line = []
-  if line.empty?
+
+def line(deli)
+  count = 1 
+  newarr = []
+  if deli.length == 0 
     puts "The line is currently empty."
   else 
-    count = 1 
-      line.each do |name|
-      current_line.push("#{count}. #{name}")
-      count += 1
-      end 
-    puts "The line is currently: #{current_line.join(" ")}"
+
+  deli.each do |name|
+    newarr.push("#{count}. #{name}")
+    count +=1 
   end 
+  puts "The line is currently: #{newarr.join(" ")}"
+end 
 end 
 
-def take_a_number(line, name)
-  line.push(name)
-  puts "Welcome, #{name}. You are number #{line.length} in line."
-end 
 
-def now_serving(line)
-  if line.empty?
-  puts "There is nobody waiting to be served!"
-  else
-    next_in_line = line.shift
-    puts "Currently serving #{next_in_line}."
-  end 
-end 
+
+
+
+
+    
+
+
+
+
+
 
 
 
